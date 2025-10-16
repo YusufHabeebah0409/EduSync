ca3a43b63616008c63bafc0adb6bd910eb25dc88
Habeebah <ojuolapehabeebah@gmail.com>
2025-10-16 16:47:55 +0100

    Enhance dashboard layout and functionality with new styles and user greeting

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { auth } from '../../firebase.config';
 import { onAuthStateChanged } from 'firebase/auth';
-import { getDatabase, ref,onValue } from 'firebase/database';
+import { getDatabase, ref, onValue } from 'firebase/database';
 import { Router } from '@angular/router';
 
 @Component({
@@ -12,20 +12,23 @@ import { Router } from '@angular/router';
   styleUrl: './dashboard.css'
 })
 export class Dashboard implements OnInit {
-  userName:any = ''
+  database = getDatabase();
+
+  userName: any = ''
   public route = inject(Router)
   ngOnInit(): void {
-    // onAuthStateChanged(auth, (user) => {
-    //   if (user) {
-    //     this.userName = user.displayName;
-    //     // console.log(user);
-        
-    //   } else {
-    //     // User is signed out
-    //     this.route.navigate(['signIn'])
-    //   }
-    // });
-
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        const dataRef = ref(this.database, 'users/0' + uid);
+        onValue(dataRef, (snapshot) => {
+          const data = snapshot.val();
+          this.userName = data.userName;
+        });
+      } else {
+        this.route.navigate(['signIn'])
+      }
+    });
   }
 
   signOut() {
